6f5b3eee9dd240847013a5937809adfb606fc50c
asimsaeed <asimsaeed353@gmail.com>
2025-11-07 15:11:12 +0500

    37. Exercise: Create a Configurable Component

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -16,8 +17,13 @@ export class AppComponent {
 
   // since we are going to access the users in the app.component.html so define the property here.
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get getSelectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(id: string){
-    console.log("Selected User id is " + id);
+    this.selectedUserId = id;
   }
 }
