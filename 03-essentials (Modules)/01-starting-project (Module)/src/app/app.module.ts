43277a344d917b4bc6d95e7d0eb17681195ad098
asimsaeed <asimsaeed353@gmail.com>
2025-11-10 15:41:24 +0500

    69. Declaring & Using Components in Angular Modules

--- 03-essentials (Modules)/01-starting-project (Module)/src/app/app.module.ts
+++ 03-essentials (Modules)/01-starting-project (Module)/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';   
+
 
 @NgModule({
     declarations: [AppComponent], // this array will contain all the components of this module
     bootstrap: [AppComponent], // bootstrap component to load the application from
+    imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // this array includes the standalone components and other imported modules
 })
 export class AppModule {
 
