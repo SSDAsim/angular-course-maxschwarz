861bbe876129822be6292d5512a2f734a8adfb68
asimsaeed <asimsaeed353@gmail.com>
2025-11-12 18:09:07 +0500

    148. An Easier Way of Setting Up Custom Two-Way Binding

--- 06-components-and-templates - deep dive/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ 06-components-and-templates - deep dive/19-custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,27 +9,29 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  // take size as input
+  /*
   @Input({required: true}) size!: {width: string;height:string};
   @Output() sizeChange = new EventEmitter<{width: string;height:string}>();
   
-  /*
-  * in order to reset the size values, we can do that in onReset() by usually setting the width and height a specific value but that is not how we should do it. we should rather use Two Way Binding 
-  * to implement Two Way Binding, we should use both @Input() and @Output, that is how Two Way Binding works.  
-  * now for angular to understand to way binding the name of the @Output has to be very specific. 
-  * if @Input() name is 'size', the @Output() must be 'sizeChange'. This @Input() and @Output() together makes Two-Way Bindable size property
-   */
-  
   onReset() {
-    //this.size = {
-    //   width: '100',
-    //   height: '200',
-    // }
-    // the above code is right but not uses Two Way Binding 
 
     this.sizeChange.emit({
       width: '100',
       height: '100',
+    });
+  }
+
+  */
+
+  /* Modern Way of Two-Way Binding (using Model Signal) */
+  /* for Angular >= 17 */
+  size = model.required<{width: string;height:string}>();
+  /* size is now signal and in template file access or read it as size() */
+
+  onReset() {
+    this.size.set({
+      width: '100',
+      height: '100'
     });
   }
 }
