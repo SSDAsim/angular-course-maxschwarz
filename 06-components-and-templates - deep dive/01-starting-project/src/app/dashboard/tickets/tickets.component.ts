2c98a8e58c0ab12098ede593d47bd2d09e2f0fc1
asimsaeed <asimsaeed353@gmail.com>
2025-11-12 15:29:42 +0500

    138. TypeScript & Type Models: Managing Tickets with Component Properties and Union Types

--- 06-components-and-templates - deep dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
+++ 06-components-and-templates - deep dive/01-starting-project/src/app/dashboard/tickets/tickets.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
+import { Ticket } from './ticket.model';
+
 @Component({
   selector: 'app-tickets',
   standalone: true,
@@ -9,5 +11,7 @@ import { NewTicketComponent } from "./new-ticket/new-ticket.component";
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
-
+  tickets: Ticket[] = [];
+  /* newticketcomponent will recieve the tickets input data, we the output that data and accept the data in the above variable */
+  /* we could use Service but we need to do it using the Components */
 }
