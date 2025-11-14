acb68f02ed81258ce986deded4cd28b1bd9f901d
asimsaeed <asimsaeed353@gmail.com>
2025-11-14 12:05:01 +0500

    176. Creating a Service

---
+++ 09-services/01-starting-project/src/app/tasks/tasks.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, signal } from '@angular/core';
+import { Task } from './task.model';
+
+@Injectable({
+    providedIn: 'root', // makes it accessible from anywhere in the app
+})
+export class TasksService {
+    tasks = signal<Task[]>([]);
+
+    onAddTask(taskData: {title: string, description: string}){
+        const newTask: Task = {
+            ...taskData, 
+            id: Math.random().toString(),
+            status: 'OPEN'
+        }
+
+        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    }
+
+}
