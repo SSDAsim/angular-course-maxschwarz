046f5958a485abb336fa55222197fab247ff01e6
asimsaeed <asimsaeed353@gmail.com>
2025-11-19 12:36:12 +0500

    197. Angular Optimization and Avoiding Angular Pollution

---
+++ 10-change-detection/01-starting-project/src/app/counter/counter.component.ts
@@ -0,0 +1,42 @@
+import { Component, signal, NgZone, inject, OnInit } from '@angular/core';
+
+import { InfoMessageComponent } from '../info-message/info-message.component';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css',
+  imports: [InfoMessageComponent],
+})
+export class CounterComponent implements OnInit {
+  private zone = inject(NgZone);
+  count = signal(0);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    // this setTimeout method does not change any value in the application so it should not be under angular's change detection radar. 
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Let\'s opt out of the zone change detection radar.')
+      }, 5000);
+    });
+    
+  }
+
+  onDecrement() {
+    this.count.update((prevCount) => prevCount - 1);
+  }
+
+  onIncrement() {
+    this.count.update((prevCount) => prevCount + 1);
+  }
+}
