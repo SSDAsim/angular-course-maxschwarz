cfdfff456dd8a51bb451c23c786e961ccfd02943
asimsaeed <asimsaeed353@gmail.com>
2025-11-21 15:09:26 +0500

    250. Syncing Reactive Form Definition & Template

--- 13-forms/01-starting-project/src/app/auth/login/login.component.ts
+++ 13-forms/01-starting-project/src/app/auth/login/login.component.ts
@@ -1,56 +1,20 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private destroyRef = inject(DestroyRef);
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  private form = viewChild.required<NgForm>('form');
-  constructor() {
-    afterNextRender(() => {
-      /* get the email of the user */
-      const savedForm = window.localStorage.getItem('saved-login-form');
+  onSubmit() {
 
-      if(savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const savedEmail = loadedFormData.email;
-
-        // this.form().setValue({
-        //   email: savedEmail, 
-        //   password: ''
-        // });
-
-        // alternatively 
-        // wait for the form to be fully initialized
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedEmail);
-        }, 1);
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email }))
-      });
-
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-    });
-  }
-  onSubmitForm(formData: NgForm){
-
-    if(formData.form.invalid){
-      return ;
-    }
-
-    const email = formData.form.value.email;
-    const password = formData.form.value.password;
-    console.log(formData);
-
-    formData.form.reset();
   }
 }
