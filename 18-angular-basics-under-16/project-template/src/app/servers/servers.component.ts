5408cf5f7e9bdff5b3f34517bdc17b4ce7b25a35
asimsaeed <asimsaeed353@gmail.com>
2025-11-26 15:47:07 +0500

    340. Working with Component Templates

---
+++ 18-angular-basics-under-16/project-template/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
+  // you have to have either 'templateUrl or template' property 
+  styleUrl: './servers.component.css',
+})
+export class ServersComponent {
+
+}
